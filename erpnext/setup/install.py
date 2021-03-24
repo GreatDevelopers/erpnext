eda99fcd0b3353641de22bbe49873c725c8fabca
SurajDadral <dadralj18@gmail.com>
2021-03-24 05:09:32 -0400

    Sets custom footer for email and website

--- erpnext/setup/install.py
+++ erpnext/setup/install.py
@@ -11,8 +11,10 @@ from frappe.desk.page.setup_wizard.setup_wizard import add_all_roles_to
 from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 from erpnext.setup.default_energy_point_rules import get_default_energy_point_rules
 
-default_mail_footer = """<div style="padding: 7px; text-align: right; color: #888"><small>Sent via
-	<a style="color: #888" href="http://erpnext.org">ERPNext</a></div>"""
+# default_mail_footer = """<div style="padding: 7px; text-align: right; color: #888"><small>Sent via
+# 	<a style="color: #888" href="http://erpnext.org">ERPNext</a></div>"""
+default_mail_footer = """<div style="padding: 7px; text-align: right; color: #888"><small>Testing and Consultancy Cell of Guru Nanak Dev Engineering College,</br>
+	Ludhiana: Serving technically since 1979.</div>"""
 
 
 def after_install():
